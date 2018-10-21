e95c405b528865334108a4143fffe797cc21257a
Sakib Ahmed <sakibi.ahmed@yahoo.com>
2018-10-21 17:26:32 -0400

    First commit for hello world program

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello{
+
+	public static void main(String[] args) {
+		allFib(7);
+	}
+	
+	static void allFib(int n) {
+		for(int i = 0; i<n; i++) {
+			System.out.println(i + ": " + fib(i));
+		}
+	}
+	
+	static int fib(int n) {
+		if(n<=0) return 0;
+		else if (n ==1) return 1;
+		return fib(n-1) + fib(n-2);
+	}
+}
